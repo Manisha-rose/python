96e8a46990d0ba748b3bbc288e67da174ef1a61e
Manisha-rose <136997259+Manisha-rose@users.noreply.github.com>
2025-10-24 14:26:54 +0530

    completed

---
+++ record14b/Graphics/Graphics3D/cuboid.py
@@ -0,0 +1,7 @@
+#cuboid.py
+
+def area(l,b,h):
+    return 2 * (( l * b ) + ( l* h ) + ( b* h ))
+
+def perimeter(l,b,h):
+    return 4 * (l + b + h)
---
+++ record14b/Graphics/rectangle.py
@@ -0,0 +1,7 @@
+# Rectangle.py
+
+def area(length, breadth):
+    return length * breadth
+
+def perimeter(length, breadth):
+    return 2 * (length + breadth)
